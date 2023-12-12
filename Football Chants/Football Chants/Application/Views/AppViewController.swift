aca65f0ec47514aaf038926369175276ddfde750
B e n g i <bambang.trd17@gmail.com>
2023-12-12 15:03:37 +0700

    Add base project wihout Storyboards concept

---
+++ Football Chants/Football Chants/Application/Views/AppViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppViewController.swift
+//  Football Chants
+//
+//  Created by Bambang Tri Rahmat Doni on 12/12/23.
+//
+
+import UIKit
+
+class AppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .orange
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
